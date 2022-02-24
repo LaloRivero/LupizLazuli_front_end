c2eecc2954a395188a34ab33ecdd338465e529c9
Eduardo Rivero <lalo@lalorivero.com>
2022-02-24 14:23:21 -0700

    Adding the Router for the application

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 
 function App() {
   return (
-    <React.Fragment className="App">
-      <Home/>
-    </React.Fragment>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+      </Routes>
+    </Router>
   );
 }
 
