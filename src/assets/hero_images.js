6966ff033f9bf22663af129772b641600255bf34
Eduardo Rivero <lalo@lalorivero.com>
2022-02-24 13:53:38 -0700

    list of images for the hero carousel

---
+++ src/assets/hero_images.js
@@ -0,0 +1,28 @@
+const images = [
+  {
+    url: "https://res.cloudinary.com/lupislazuli-fotografia/image/upload/v1630613940/Lilette%20y%20Dalia/Lilette_y_Dalia_Boda_pre1-30_kff9ak.jpg",
+    name: "Lilette&Dalia",
+  },
+  {
+    url: "https://res.cloudinary.com/lupislazuli-fotografia/image/upload/v1630613172/MyO_Casual_HD-15_noe5fk.jpg",
+    name: "MyO_casual",
+  },
+  {
+    url: "https://res.cloudinary.com/lupislazuli-fotografia/image/upload/v1630613169/MyO_Casual_HD-78_qb7x82.jpg",
+    name: "MyO_casual",
+  },
+  {
+    url: "https://res.cloudinary.com/lupislazuli-fotografia/image/upload/v1630613168/LovelyDayMajalca-56_qojzom.jpg",
+    name: "LovelyDayMajalca",
+  },
+  {
+    url: "https://res.cloudinary.com/lupislazuli-fotografia/image/upload/v1630613160/Ale_e_Ivan_Preboda_HD-127_ersksr.jpg",
+    name: "Ale&Ivan",
+  },
+  {
+    url: "https://res.cloudinary.com/lupislazuli-fotografia/image/upload/v1630613988/Eunice%20y%20Daniel/EyD_web_pre-25_ucuvcj.jpg",
+    name: "Eunice y Daniel",
+  },
+];
+
+export default images
