a631f6cd38285855963bfdf82e5082f16913574c
Eduardo Rivero <lalo@lalorivero.com>
2022-02-24 14:14:23 -0700

    Adding second section of the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import PhotoCarousel from '../components/PhotoCarousel';
+import SectionCard from '../components/SectionCard';
+
+
 const Home = () => {
   return (
     <React.Fragment>
       <PhotoCarousel/>
+      <div>
+        <img src="https://res.cloudinary.com/lupislazuli-fotografia/image/upload/v1630614425/Sofia%20y%20Julienne/SyJ_Boda_Pre-4_yiv2uk.jpg" alt="image" />
+        <SectionCard/>
+      </div>
     </React.Fragment>
   );
 }
