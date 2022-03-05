edf60ea8b290d0ad6045016ec3649a1d760ccf6a
Eduardo Rivero <lalo@lalorivero.com>
2022-03-04 20:25:13 -0700

    texts for different sections

---
+++ src/assets/text.js
@@ -0,0 +1,7 @@
+const text= {
+  section1_text:{
+    "text":"Recuerda tu día tal cual lo viviste… abrazar a los que amas, sonreír como nunca, derramar lágrimas felices, bailar hasta el cansancio, ¡celebrar el amor que te rodea!"
+  }
+}
+
+export default text
