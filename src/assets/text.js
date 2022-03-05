ccd97e16b06147f143d06400d41fd760bc064e6e
Eduardo Rivero <lalo@lalorivero.com>
2022-03-04 20:35:39 -0700

    text section 1

--- src/assets/text.js
+++ src/assets/text.js
@@ -1,7 +1,7 @@
-const text= {
-  section1_text:{
-    "text":"Recuerda tu día tal cual lo viviste… abrazar a los que amas, sonreír como nunca, derramar lágrimas felices, bailar hasta el cansancio, ¡celebrar el amor que te rodea!"
-  }
-}
+const text = {
+  section1_text: {
+    text: "Recuerda tu día tal cual lo viviste… abrazar a los que amas, sonreír como nunca, derramar lágrimas felices, bailar hasta el cansancio, ¡celebrar el amor que te rodea!",
+  },
+};
 
-export default text
+export default text;
