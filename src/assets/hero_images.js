610ac3325191710b24f2c68e22c22fc50b7c70fc
Eduardo Rivero <lalo@lalorivero.com>
2022-03-04 20:22:33 -0700

    Photographer selection

--- src/assets/hero_images.js
+++ src/assets/hero_images.js
@@ -1,19 +1,27 @@
 const images = [
   {
-    url: "https://res.cloudinary.com/lupislazuli-fotografia/image/upload/v1630613940/Lilette%20y%20Dalia/Lilette_y_Dalia_Boda_pre1-30_kff9ak.jpg",
+    url: "https://res.cloudinary.com/lupislazuli-fotografia/image/upload/v1646355390/TOP%206/Ale_e_Ivan_TTD_WEB-131_oanmbd.jpg",
+    name: "Ale&Ivan",
+  },
+  {
+    url: "https://res.cloudinary.com/lupislazuli-fotografia/image/upload/v1646355178/TOP%206/LyF_postboda_preweb-6_wee9uf.jpg",
+    name: "L&F",
+  },
+  {
+    url: "https://res.cloudinary.com/lupislazuli-fotografia/image/upload/v1646353126/TOP%206/Dalia_Lilette_Boda_web-793_da16mq.jpg",
     name: "Lilette&Dalia",
   },
   {
-    url: "https://res.cloudinary.com/lupislazuli-fotografia/image/upload/v1630613940/Lilette%20y%20Dalia/Lilette_y_Dalia_Boda_pre1-30_kff9ak.jpg",
-    name: "Lilette&Dalia",
+    url: "https://res.cloudinary.com/lupislazuli-fotografia/image/upload/v1646352928/TOP%206/P_D_boda_pre_-_web-10_ncehso.jpg",
+    name: "P&D",
   },
   {
-    url: "https://res.cloudinary.com/lupislazuli-fotografia/image/upload/v1630613940/Lilette%20y%20Dalia/Lilette_y_Dalia_Boda_pre1-30_kff9ak.jpg",
-    name: "Lilette&Dalia",
+    url: "https://res.cloudinary.com/lupislazuli-fotografia/image/upload/v1646352870/TOP%206/Mariana_y_Victor_PRE_web-12_ovfiq5.jpg",
+    name: "Mariana&Victor",
   },
   {
-    url: "https://res.cloudinary.com/lupislazuli-fotografia/image/upload/v1630613940/Lilette%20y%20Dalia/Lilette_y_Dalia_Boda_pre1-30_kff9ak.jpg",
-    name: "Lilette&Dalia",
+    url: "https://res.cloudinary.com/lupislazuli-fotografia/image/upload/v1646352678/TOP%206/Boda_2nd_Cristy-16_pee43x.jpg",
+    name: "Cristy",
   },
 ];
 
